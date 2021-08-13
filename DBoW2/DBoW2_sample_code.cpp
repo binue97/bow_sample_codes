836012f9a7203fff56c2c3a5a713eeb3e37371c4
binue97 <ko040123@gmail.com>
2021-08-13 15:29:11 +0900

    style: Fixed coding style [DBoW2]
    - Deleted unnecessary comments
    - Rearranged headers
    - Deleted using namespace std
    - Aliased vector<vector<Mat>> , vector<Mat>
    - Used pass-by-reference instead of pass-by-pointer

--- DBoW2/DBoW2_sample_code.cpp
+++ DBoW2/DBoW2_sample_code.cpp
@@ -1,92 +1,63 @@
-/**
- * File: Demo.cpp
- * Date: November 2011
- * Author: Dorian Galvez-Lopez
- * Description: demo application of DBoW2
- * License: see the LICENSE.txt file
- */
+#include "DBoW2.h"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <easy/profiler.h>
 
 #include <iostream>
 #include <vector>
 #include <iomanip>
 
-
-// DBoW2
-#include "DBoW2.h" // defines OrbVocabulary and OrbDatabase
-
-// OpenCV
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/features2d.hpp>
-
-#include <easy/profiler.h>
-
 using namespace DBoW2;
-using namespace std;
+using DescriptorVector = std::vector<std::vector<cv::Mat>>;
+using Descriptors = std::vector<cv::Mat>;
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-void loadDBFeatures(vector<vector<cv::Mat > > &features);
-void loadVocabulary(OrbVocabulary* vobabulary);
-void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out);
-void createDatabase(OrbDatabase* database, const vector<vector<cv::Mat > > &features);
-void loadQueryFeatures(vector<vector<cv::Mat > > &features);
-void queryDatabase(OrbDatabase* database, const vector<vector<cv::Mat > > &features, vector<int> &imgIdx);
-void saveResult(vector<int> &imgIdx);
+void loadDBFeatures(DescriptorVector &vfeatures);
+void loadVocabulary(OrbVocabulary &voc);
+void changeStructure(const cv::Mat &plain, Descriptors &out);
+void createDatabase(OrbDatabase &db, const DescriptorVector &vfeatures);
+void loadQueryFeatures(DescriptorVector &vfeatures);
+void queryDatabase(OrbDatabase &db, const DescriptorVector &vfeatures, std::vector<int> &imgIdx);
+void saveResult(std::vector<int> &imgIdx);
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 // Number of Images to Build Database
 const int NDBIMAGES = 4;
 // Number of Images to Query with Database
 const int NQUERYIMAGES = 1;
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 // Paths 
-const string vocPath = "../ORBvoc/ORBvoc.txt";
-const string dbPath = "../demo/Database/";
-const string queryPath = "../demo/Query/";
+const std::string vocPath = "../ORBvoc/ORBvoc.txt";
+const std::string dbPath = "../demo/Database/";
+const std::string queryPath = "../demo/Query/";
 
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-void wait()
-{
-  cout << endl << "Press enter to save results..." << endl;
-  getchar();
-}
-
-// ----------------------------------------------------------------------------
 
 int main()
 {
-  // Initiate Profiler
   EASY_MAIN_THREAD;
   EASY_PROFILER_ENABLE;
 
   // DataStructures
-  vector<int> imgIndex;
-  vector<vector<cv::Mat > > dbFeatures;
-  vector<vector<cv::Mat > > queryFeatures;
+  std::vector<int> imgIndex;
+  DescriptorVector dbFeatures;
+  DescriptorVector queryFeatures;
   OrbVocabulary* ptrVocabulary = new OrbVocabulary();
 
   loadDBFeatures(dbFeatures);
 
-  // Load Vocabulary
-  loadVocabulary(ptrVocabulary);
+  loadVocabulary(*ptrVocabulary);
   
   EASY_BLOCK("Initialize Database", profiler::colors::LightBlue);
   OrbDatabase* ptrDatabase = new OrbDatabase(*ptrVocabulary, false, 0);
   EASY_END_BLOCK;
 
-  createDatabase(ptrDatabase, dbFeatures);
+  createDatabase(*ptrDatabase, dbFeatures);
 
   loadQueryFeatures(queryFeatures);
 
-  queryDatabase(ptrDatabase, queryFeatures, imgIndex);
-
-  wait();
+  queryDatabase(*ptrDatabase, queryFeatures, imgIndex);
 
   saveResult(imgIndex);
 
@@ -94,162 +65,135 @@ int main()
   return 0;
 }
 
-// ----------------------------------------------------------------------------
 
-void loadDBFeatures(vector<vector<cv::Mat > > &features)
+void loadDBFeatures(DescriptorVector &vfeatures)
 {
-  EASY_FUNCTION("Load DB Features", profiler::colors::LightGreen);
+  EASY_FUNCTION("Load DB Descriptors", profiler::colors::LightGreen);
 
-  features.clear();
-  features.reserve(NDBIMAGES);
+  vfeatures.clear();
+  vfeatures.reserve(NDBIMAGES);
 
   cv::Ptr<cv::ORB> orb = cv::ORB::create();
 
-  cout << " < Feature Extraction for DB Images >" << endl;
+  // Feature Extraction for Database Images 
   for(int i = 0; i < NDBIMAGES; ++i)
   {
-    //if(i % 500 == 0) cout << "Extracting Features from " << i << "th Image ... " << endl; 
-
-    stringstream ss;
-    ss << dbPath << setfill('0') << setw(6) << i << ".png";
+    std::stringstream ss;
+    ss << dbPath << std::setfill('0') << std::setw(6) << i << ".png";
 
     cv::Mat image = cv::imread(ss.str(), 0);
     cv::Mat mask;
-    vector<cv::KeyPoint> keypoints;
+    std::vector<cv::KeyPoint> keypoints;
     cv::Mat descriptors;
 
     orb->detectAndCompute(image, mask, keypoints, descriptors);
 
-    features.push_back(vector<cv::Mat >());
-    changeStructure(descriptors, features.back());
+    vfeatures.push_back(Descriptors());
+    changeStructure(descriptors, vfeatures.back());
   }
 }
 
-// ----------------------------------------------------------------------------
 
-void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out)
+void changeStructure(const cv::Mat &plain, Descriptors &out)
 {
   out.resize(plain.rows);
 
   for(int i = 0; i < plain.rows; ++i)
-  {
     out[i] = plain.row(i);
-  }
+  
 }
 
-// ----------------------------------------------------------------------------
 
-void loadVocabulary(OrbVocabulary* voc)
+void loadVocabulary(OrbVocabulary &voc)
 {
   EASY_FUNCTION("Load Vocabulary", profiler::colors::Yellow);
-
-  cout << endl << "< Loading ORB Vocabulary >" << endl;
-
-  bool bVocLoad = voc->loadFromTextFile(vocPath);
-  if(!bVocLoad)
+  
+  if(!(voc.loadFromTextFile(vocPath)))
   {
-      cerr << "Wrong path to vocabulary. " << endl;
-      cerr << "Falied to open at: " << vocPath << endl;
+      std::cerr << "Wrong path to vocabulary. " << std::endl;
+      std::cerr << "Falied to open at: " << vocPath << std::endl;
       exit(-1);
   }
 
-  cout << endl << "< Vocabulary information >" << endl
-  << *voc << endl << endl;
+  std::cout << "< Vocabulary information >" << std::endl
+  << voc << std::endl << std::endl;
 
-  return;
 }
 
-// ----------------------------------------------------------------------------
 
-void createDatabase(OrbDatabase* db, const vector<vector<cv::Mat > > &features)
+void createDatabase(OrbDatabase &db, const DescriptorVector &vfeatures)
 {
   EASY_FUNCTION("Create Database", profiler::colors::Magenta);
 
-  cout << "< Creating a Database >" << endl;
-
-  // add images to the database
   for(int i = 0; i < NDBIMAGES; i++)
-  {
-    int idx = db->add(features[i]);
-    //if(idx % 500 == 0) cout << "Adding " << idx << "th image to Database ... " << endl;
-  }
+    db.add(vfeatures[i]);
 
-  cout << endl << "< Database information >" << endl << *db << endl << endl;
+  std::cout << "< Database information >" << std::endl << db << std::endl << std::endl;
 
-  return;
 }
 
-// ----------------------------------------------------------------------------
 
-void loadQueryFeatures(vector<vector<cv::Mat > > &features)
+void loadQueryFeatures(DescriptorVector &vfeatures)
 {
-  EASY_FUNCTION("Load Query Features", profiler::colors::DeepOrange);
+  EASY_FUNCTION("Load Query Descriptors", profiler::colors::DeepOrange);
 
-  features.clear();
-  features.reserve(NQUERYIMAGES);
+  vfeatures.clear();
+  vfeatures.reserve(NQUERYIMAGES);
 
   cv::Ptr<cv::ORB> orb = cv::ORB::create();
 
-  cout << "< Feature Extraction for Query Images > " << endl << endl;
+  // Feature Extraction for Query Images 
   for(int i = 0; i < NQUERYIMAGES; ++i)
   {
-    stringstream ss;
+    std::stringstream ss;
     
-    ss << queryPath << setfill('0') << setw(6) << i << ".png";
+    ss << queryPath << std::setfill('0') << std::setw(6) << i << ".png";
 
     cv::Mat image = cv::imread(ss.str(), 0);
     cv::Mat mask;
-    vector<cv::KeyPoint> keypoints;
+    std::vector<cv::KeyPoint> keypoints;
     cv::Mat descriptors;
 
     orb->detectAndCompute(image, mask, keypoints, descriptors);
 
-    features.push_back(vector<cv::Mat >());
-    changeStructure(descriptors, features.back());
+    vfeatures.push_back(Descriptors());
+    changeStructure(descriptors, vfeatures.back());
   }
 
 }
 
-// ----------------------------------------------------------------------------
 
-void queryDatabase(OrbDatabase* db, const vector<vector<cv::Mat > > &features, vector<int> &imgIdx)
+void queryDatabase(OrbDatabase &db, const DescriptorVector &vfeatures, std::vector<int> &imgIdx)
 {
   EASY_FUNCTION("Query Database", profiler::colors::DarkTeal);
-
-  cout << "< Querying the database > " << endl;
 
   QueryResults ret;
   for(int i = 0; i < NQUERYIMAGES; i++)
   {
     // Query & Save 4 best match in QueryResult
-    db->query(features[i], ret, 4);
+    db.query(vfeatures[i], ret, 4);
 
-    cout << "Searching for Image " << i << ". " << ret << endl << endl;
+    std::cout << "Searching for Image " << i << ". " << ret << std::endl << std::endl;
 
     // Save Image index of the Best Match for each Query Image
     imgIdx.push_back(ret[0].Id);
   }
 
-  cout << endl;
-
-  return;
 }
 
-// ----------------------------------------------------------------------------
 
-void saveResult(vector<int> &imgIdx)
+void saveResult(std::vector<int> &imgIdx)
 {
   // Search Image by index and Save 
   for(int i = 0; i < NQUERYIMAGES; i++)
   {
-    stringstream ssR;
-    ssR << dbPath << setfill('0') << setw(6) << imgIdx[i] << ".png";
+    std::stringstream ssR;
+    ssR << dbPath << std::setfill('0') << std::setw(6) << imgIdx[i] << ".png";
     cv::Mat image = cv::imread(ssR.str(), 0);
 
-    stringstream ssW;
+    std::stringstream ssW;
     ssW << queryPath << "Result" << i << ".png";
     cv::imwrite(ssW.str(), image);
   }
 
-  return;
 }
